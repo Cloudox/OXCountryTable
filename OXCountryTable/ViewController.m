623492ac2bdb84b60d5cd66268e54f30e3a8ccb2
Cloudox <cloudox@foxmail.com>
2017-08-09 17:13:25 +0800

    完成国家（含国旗）列表，将中国放在第一行

--- OXCountryTable/ViewController.m
+++ OXCountryTable/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSMutableArray *countryArray;
+@property (nonatomic, strong) NSMutableArray *countryCodeArray;
 
 @end
 
@@ -16,9 +19,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.countryArray = [[NSMutableArray alloc] init];
+    self.countryCodeArray = [[NSMutableArray alloc] init];
+    NSLocale *locale = [NSLocale currentLocale];
+    NSArray *countryArray = [NSLocale ISOCountryCodes];
+    for (NSString *countryCode in countryArray) {
+        if ([countryCode isEqualToString:@"CN"]) {
+            [self.countryCodeArray insertObject:countryCode atIndex:0];
+            NSString *displayNameString = [locale displayNameForKey:NSLocaleCountryCode value:countryCode];
+            [self.countryArray insertObject:displayNameString atIndex:0];
+            continue;
+        }
+        [self.countryCodeArray addObject:countryCode];
+        NSString *displayNameString = [locale displayNameForKey:NSLocaleCountryCode value:countryCode];
+        [self.countryArray addObject:displayNameString];
+        
+    }
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.tableFooterView = [[UIView alloc] init];
+    [self.view addSubview:tableView];
 }
 
+#pragma mark - UITableView Datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.countryArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSString *SimpleCell = @"SimpleCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: SimpleCell];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SimpleCell];
+        cell.backgroundColor = [UIColor clearColor];
+    }
+    
+    cell.textLabel.text = self.countryArray[indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:self.countryCodeArray[indexPath.row]];
+    
+    return cell;
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
